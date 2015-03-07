b2528d811c311a063b6496cc21694388ae586b2a
ocaraballero <ocaraballero@ocaraballeroptl>
2015-03-07 17:16:24 +0000

    login parece que va

---
+++ control/PerfilController.php
@@ -0,0 +1,10 @@
+<?php
+//require_once 'model/DatabaseManager.php';
+require_once 'Controller.php';
+
+class PerfilController extends Controller {
+
+    function process() {
+        $this->_view->render(array());
+    }
+}
---
+++ view/PerfilView.php
@@ -0,0 +1,11 @@
+<h1>HELLO</h1>
+
+
+<script>
+    //$( document ).on("load",function() {
+    var iable = "<a href=\"#\" class=\"ui-btn \"></a>";
+    var iable2 = "<a href=\"Inicio\" class=\"ui-btn ui-corner-all ui-icon-bars ui-btn-icon-notext ui-nodisc-icon ui-alt-icon\">Menu</a>";
+    $("#header").append(iable);
+    $("#header").append(iable2);    
+    //});
+</script>
