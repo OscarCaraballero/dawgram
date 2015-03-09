afad72623a56909dc910d7ea1952a1a2f73c38e8
ocaraballero <ocaraballero@ocaraballero-PC>
2015-03-09 21:54:12 +0000

    Footer

---
+++ view/footer.php
@@ -0,0 +1,11 @@
+<div data-role="footer">
+        <div data-role="navbar">
+            <ul>
+                <li><a href="#" data-icon="home"></a></li>
+                <li><a href="#" data-icon="search" class="ui-btn-active"></a></li>
+                <li><a href="#" data-icon="camera"></a></li>
+                <li><a href="#" data-icon="heart"></a></li>
+                <li><a href="#" data-icon="user"></a></li>
+            </ul>
+        </div><!-- /navbar -->
+</div><!-- /footer -->
